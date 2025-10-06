d1d1424a78a9bb1f77d6f3e621d77f1bd57c88ef
Fernando Quilaqueo <167574791+Guaygod@users.noreply.github.com>
2025-10-06 19:47:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ app = Flask(__name__)  # Usa 'templates/' por defecto
 @app.route('/')
 def home():
     return render_template('index.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
