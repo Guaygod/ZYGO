cecaee7324bef6a8acf11d383199475c4b77dc89
Fernando Quilaqueo <167574791+Guaygod@users.noreply.github.com>
2025-10-06 01:46:34 -0300

    Change home route to render zygo.html

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__, static_folder='.', template_folder='.')
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('zygo.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
